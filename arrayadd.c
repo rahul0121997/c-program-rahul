f07ed3ae38af6f2cbcafa02d4445c5eceeadfcf3
rahul0121997 <rahul0121997@gmail.com>
2025-02-15 12:58:21 +0530

    15-02-2025

--- arrayadd.c
+++ arrayadd.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<conio.h>
-
+/*taking input fron user and print that number with addition*/
 int main()
 {
-    int n;
+    int n ,sum=0;
     printf("enter number N:");
     scanf("%d",&n);
     int a[n];
@@ -11,11 +11,13 @@ int main()
     for(int i = 0;i < n; i++)
     {
         scanf("%d",&a[i]);
+        sum=sum+a[i];
     }
     printf("the number you enterd is:\n");
     for (int i = 0;i < n; i++)
     {
-        printf("%d \n",a[i]);
+        printf("%d\n",a[i]);
     }
+    printf("the addition:%d \n",sum);
     return 0;
 }
